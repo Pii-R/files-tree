350c642efe7f1b6a9746df84775f4d30a28d55da
Pierre <phenry.pm@gmail.com>
2023-01-21 15:04:19 +0100

    chore: remove unnecessary files

--- build/tests/tree.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tree_1 = require("../tree");
-test("list of files", () => {
-    expect((0, tree_1.ListFilesFromFolder)("tests/test_folder")).toEqual([
-        "env.test",
-        "file.txt",
-    ]);
-});
--- build/tree.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ListFilesFromFolder = void 0;
-const fs = require("fs");
-function ListFilesFromFolder(folder) {
-    return fs.readdirSync("src/tests/test_folder");
-}
-exports.ListFilesFromFolder = ListFilesFromFolder;
